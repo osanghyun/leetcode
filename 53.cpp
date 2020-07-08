3d341839ea929a1e3e38cef5a738427a7b6993bd
OhSangHyun <67955993+osanghyun@users.noreply.github.com>
2020-07-08 17:31:09 +0900

    53.cpp
    Maximum Subarray

---
+++ 53.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int size = (int)nums.size();
+    
+        int* DP = new int[size]();
+
+        DP[0] = nums[0];
+
+        int rv=DP[0];
+
+        for(int i=1;i<size;i++){
+            DP[i] = max(DP[i-1]+nums[i],nums[i]);
+            if(rv < DP[i])
+                rv = DP[i];
+        }
+
+        delete []DP;
+
+        return rv;
+    }
+};
