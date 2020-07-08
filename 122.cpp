73a2a52e999d357e47b9ef0308d5b39f8cb11f38
OhSangHyun <67955993+osanghyun@users.noreply.github.com>
2020-07-08 17:51:30 +0900

    Best Time to Bus and Sell Stock ||
    Greedy

---
+++ 122.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int size = prices.size();
+        
+        int profit = 0;
+        
+        for(int i=1;i<size;i++){
+            if(prices[i-1] < prices[i])
+                profit += prices[i] - prices[i-1];
+        }
+        
+        return profit;
+    }
+};
