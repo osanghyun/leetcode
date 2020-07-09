6370a34bacccbd26a02dd010300dbef7555f3094
OhSangHyun <67955993+osanghyun@users.noreply.github.com>
2020-07-09 21:41:51 +0900

    Two Sum
    Brute Force

---
+++ 1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int size = nums.size();
+        
+        vector<int> v;
+        
+        for(int i=0;i<size-1;i++){
+            
+            for(int j=i+1;j<size;j++){
+                
+                if(nums[i]+nums[j] == target){
+                    
+                    v.push_back(i);
+                    v.push_back(j);
+                    
+                    return v;
+                }
+            }
+        }
+        
+        return v;
+    }
+};
